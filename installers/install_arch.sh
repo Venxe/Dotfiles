53ad18ea075382b97b3a3484aecf67488705edfa
Venxe <ivenxe@gmail.com>
2025-03-31 20:19:33 +0300

    Update install_arch.sh

--- installers/install_arch.sh
+++ installers/install_arch.sh
@@ -32,8 +32,8 @@ cp -a home/.config/* ~/.config/
 
 cp installers/wall_archlinux.png ~/Pictures/Wallpapers/pywallpaper.png
 cp installers/wall_archlinux.png ~/Pictures/Wallpapers/walls/wall_archlinux.png
-wal -i ~/Dotfiles/installers/wall_archlinux.png  
-wal -i ~/Dotfiles/installers/wall_archlinux.png -n  
+wal -i ~/installers/wall_archlinux.png  
+wal -i ~/installers/wall_archlinux.png -n  
 
 while true; do
     echo "Enter your password to change the shell to Fish:"
