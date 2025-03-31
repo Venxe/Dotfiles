3bf6f5dbc017a24ab4f618438c769dd73889be58
Venxe <ivenxe@gmail.com>
2025-03-31 14:57:49 +0300

    Update and rename install.sh to install_arch.sh

--- installers/install_arch.sh
+++ installers/install_arch.sh
@@ -8,10 +8,10 @@ cd ..
 
 sudo chmod -R 777 "$HOME"
 
-sudo pacman -S $(<packages.txt)
+sudo pacman -S $(<installers/packages.txt)
 
-yay -S python-pywal16 python-pywalfox hyprshot pyprland wlogout clock-rs-git qogir-icon-
-theme illogical-impulse-bibata-modern-classic-bin spotify gnome-network-displays 
+yay -S python-pywal16 python-pywalfox hyprshot pyprland wlogout clock-rs-git qogir-icon-theme \
+illogical-impulse-bibata-modern-classic-bin spotify gnome-network-displays
 
 sudo reflector --country "US" --country "DE" --country "TR" --country "GR" --latest 10 --sort age --save /etc/pacman.d/mirrorlist
 
@@ -23,16 +23,14 @@ sudo systemctl enable avahi-daemon
 mkdir -p ~/Desktop ~/Documents ~/Downloads ~/Music ~/Public ~/Templates ~/Videos
 mkdir -p ~/Pictures/Wallpapers/walls
 
-cp ~/Dotfiles/starship.toml ~/.config/starship.toml
-cp -a ~/Dotfiles/.config/* ~/.config/
+cp home/starship.toml ~/.config/starship.toml
+cp -a home/.config/* ~/.config/
 
-cp ~/Dotfiles/wall_archlinux.png ~/Pictures/Wallpapers/pywallpaper.png
-cp ~/Dotfiles/wall_archlinux.png ~/Pictures/Wallpapers/walls/wall_archlinux.png
-wal -i ~/Dotfiles/wall_archlinux.png
-wal -i ~/Dotfiles/wall_archlinux.png -n
+cp installers/wall_archlinux.png ~/Pictures/Wallpapers/pywallpaper.png
+cp installers/wall_archlinux.png ~/Pictures/Wallpapers/walls/wall_archlinux.png
+wal -i ~/Pictures/Wallpapers/pywallpaper.png
+wal -i ~/Pictures/Wallpapers/pywallpaper.png -n
 
 chsh -s /usr/bin/fish
-
-rm -rf "$HOME/Dotfiles"
 
 notify-send "Setup Completed" "Installation successful. Restart for changes to take effect."
