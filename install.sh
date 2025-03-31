a1232aff9815f86ea584dc35ba99278729a49213
Venxe <ivenxe@gmail.com>
2025-03-31 12:13:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,12 +25,12 @@ sudo systemctl enable avahi-daemon
 mkdir -p ~/Desktop ~/Documents ~/Downloads ~/Music ~/Public ~/Templates ~/Videos
 mkdir -p ~/Pictures/Wallpapers/walls
 
-cp ~/Dotfiles/wall_archlinux.png ~/Pictures/Wallpapers/pywallpaper.png
-cp ~/Dotfiles/wall_archlinux.png ~/Pictures/Wallpapers/walls/wall_archlinux.png
-wal -i ~/Pictures/Wallpapers/pywallpaper.png -n
-
 cp ~/Dotfiles/starship.toml ~/.config/starship.toml
 cp -a ~/Dotfiles/.config/* ~/.config/
+
+cp ~/Dotfiles/wall_archlinux.png ~/Pictures/Wallpapers/pywallpaper.png
+cp ~/Dotfiles/wall_archlinux.png ~/Pictures/Wallpapers/walls/wall_archlinux.png
+wal -i ~/Pictures/Wallpapers/pywallpaper.png
 
 rm -rf "$HOME/Dotfiles"
 
