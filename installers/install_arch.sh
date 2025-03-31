3df4d97cee9a26dc864975a1d6498a52aa81b871
Venxe <ivenxe@gmail.com>
2025-03-31 15:53:42 +0300

    Update install_arch.sh

--- installers/install_arch.sh
+++ installers/install_arch.sh
@@ -10,8 +10,8 @@ sudo chmod -R 777 "$HOME"
 
 sudo pacman -S $(<installers/packages.txt)
 
-yay -S python-pywal16 python-pywalfox hyprshot pyprland wlogout clock-rs-git qogir-icon-theme
-illogical-impulse-bibata-modern-classic-bin spotify gnome-network-displays myfetch
+yay -S python-pywal16 python-pywalfox hyprshot pyprland wlogout clock-rs-git qogir-icon-theme qalculate-gtk
+illogical-impulse-bibata-modern-classic-bin spotify gnome-network-displays myfetch proton-vpn-gtk-app kora-icon-theme 
 
 sudo reflector --country "US" --country "DE" --country "TR" --country "GR" --latest 10 --sort age --save /etc/pacman.d/mirrorlist
 
