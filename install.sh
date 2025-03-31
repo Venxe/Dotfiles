234eafe2c9ea480f4ce995f0062c5e772d792738
Venxe <ivenxe@gmail.com>
2025-03-31 12:47:27 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,17 +6,18 @@ sudo pacman -S base-devel
 makepkg -si
 cd ..
 
-chsh -s /usr/bin/fish
 sudo pacman -S cmake meson cpio pkg-config
 sudo chmod -R 777 "$HOME"
 
-yay -S reflector rsync python-pywal16 swww waybar swaync starship fastfetch neovim python-pywalfox hypridle \
+yay -S reflector rsync python-pywal16 swww waybar swaync starship fastfetch fish neovim python-pywalfox hypridle \
 hyprcursor hyprpicker hyprshot hyprlock pyprland wlogout fd cava brightnessctl clock-rs-git nerd-fonts nwg-look \
 qogir-icon-theme materia-gtk-theme illogical-impulse-bibata-modern-classic-bin thunar gvfs tumbler eza \
 bottom htop libreoffice-fresh spotify ncspot discord code blueman bluez pipewire pipewire-pulse pipewire-alsa \
 pipewire-jack pavucontrol pulsemixer gnome-network-displays gst-plugins-bad
 
 sudo reflector --country "US" --country "DE" --country "TR" --country "GR" --latest 10 --sort age --save /etc/pacman.d/mirrorlist
+
+chsh -s /usr/bin/fish
 
 systemctl enable bluetooth
 systemctl --user enable pipewire.service pipewire-pulse.service
