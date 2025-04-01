925784c835deee96335d834a772ec5ae8e6216e5
Venxe <ivenxe@gmail.com>
2025-04-01 21:08:34 +0300

    Update config.fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -10,18 +10,11 @@ end
 alias lsd='eza --icons'
 alias pacup='sudo pacman -Rns (pacman -Qdtq)'
 alias grep='grep --color=auto'
-alias pool='clear && asciiquarium'
 alias s='clear && myfetch -i s -f -c 16 -C "  "'
 alias bye='sudo shutdown -h now'
-alias loop='sudo reboot'
-alias h='dbus-launch Hyprland'
 alias fonts='fc-list -f "%{family}\n"'
-alias tasks='btm'
 alias Docs="cd ~/Documents && nvim"
 alias Settings="cd ~/.config/hypr && nvim"
-alias spot="ncspot"
-alias untar="tar -xf"
-alias n="nvim"
 
 set -gx NVM_DIR "$HOME/.nvm"
 if test -s "$NVM_DIR/nvm.sh"
