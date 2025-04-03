487f955d6f5bba4e2c8ded0fb0ce21e1b24d7d74
Venxe <ivenxe@gmail.com>
2025-04-03 17:20:37 +0300

    Update install.sh

--- installers/install.sh
+++ installers/install.sh
@@ -55,6 +55,8 @@ mkdir -p ~/Desktop ~/Documents ~/Downloads ~/Music ~/Public ~/Templates ~/Videos
 mkdir -p ~/Pictures/Wallpapers/walls
 
 echo "${CYAN}Copying configuration files...${RESET}"
+sudo cp -a dotcfg/sb-theme/* /usr/share/sddm/themes/
+sudo cp -a dotcfg/sddm.conf/* /etc/sddm.conf
 sudo cp -a dotcfg/.wofi-fix/* /usr/share/applications/
 cp -a dotcfg/.config/* ~/.config/
 
