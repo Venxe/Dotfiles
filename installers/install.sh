53ff59f0f55e6fd465a9d4960cadd17fcdaddcb9
Venxe <ivenxe@gmail.com>
2025-04-06 01:36:23 +0300

    Update install.sh

--- installers/install.sh
+++ installers/install.sh
@@ -59,8 +59,9 @@ sudo bash -c '
   cp -a dotcfg/sb-theme /usr/share/sddm/themes/ &&
   cp -a dotcfg/sddm.conf /etc/sddm.conf &&
   cp -a dotcfg/.wofi-fix /usr/share/applications/
-  bash ./lsp-hider.sh
 '
+
+bash ./lsp-hider.sh
 cp -a dotcfg/.config/* ~/.config/
 
 echo "${CYAN}Setting default wallpaper...${RESET}"
