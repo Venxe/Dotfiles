0dc48c98467b8ca3245af216ca1c3216a0883668
Venxe <ivenxe@gmail.com>
2025-04-09 20:34:06 +0300

    Delete dotcfg/.config/waybar/scripts/wlsunset-toggle.sh

--- dotcfg/.config/waybar/scripts/wlsunset-toggle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if pgrep -x "wlsunset" > /dev/null; then
-    pkill wlsunset
-else
-    # Please enter your latitude (-l) and longitude (-L) values corresponding to your actual location.
-    wlsunset -l 41.0 -L 29.0 &
-fi
