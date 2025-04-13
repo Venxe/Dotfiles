635765cfcc202cb1d12225d4a43afbf982e34806
Venxe <ivenxe@gmail.com>
2025-04-13 20:16:43 +0300

    Update colorpicker.sh

--- dotcfg/.config/waybar/scripts/colorpicker.sh
+++ dotcfg/.config/waybar/scripts/colorpicker.sh
@@ -1,52 +1,56 @@
 #!/usr/bin/env bash
+
 check() {
-  command -v "$1" 1>/dev/null
+  command -v "$1" &>/dev/null
 }
 
 loc="$HOME/.cache/colorpicker"
-[ -d "$loc" ] || mkdir -p "$loc"
-[ -f "$loc/colors" ] || touch "$loc/colors"
+mkdir -p "$loc"
+touch "$loc/colors"
 
 limit=10
 
-[[ $# -eq 1 && $1 = "-l" ]] && {
+if [[ $# -eq 1 && $1 == "-l" ]]; then
   cat "$loc/colors"
   exit
-}
+fi
 
-[[ $# -eq 1 && $1 = "-j" ]] && {
+if [[ $# -eq 1 && $1 == "-j" ]]; then
   text="$(head -n 1 "$loc/colors")"
+  [ -z "$text" ] && text="#ffffff"
 
   mapfile -t allcolors < <(tail -n +2 "$loc/colors")
-  # allcolors=($(tail -n +2 "$loc/colors"))
-  tooltip="<b>   COLORS</b>\n\n"
 
-  tooltip+="-> <b>$text</b>  <span color='$text'></span>  \n"
-  for i in "${allcolors[@]}"; do
-    tooltip+="   <b>$i</b>  <span color='$i'></span>  \n"
+  tooltip="<b>   COLORS</b>\\n\\n"
+  tooltip+="-> <b>$text</b>  <span color='$text'></span>\\n"
+
+  for color in "${allcolors[@]}"; do
+    tooltip+="   <b>$color</b>  <span color='$color'></span>\\n"
   done
 
-  cat <<EOF
-{ "text":"<span color='$text'></span>", "tooltip":"$tooltip"}  
-EOF
-
+  printf '{ "text": "<span color='\''%s'\''></span>", "tooltip": "%s" }\n' "$text" "$tooltip"
   exit
-}
+fi
 
 check hyprpicker || {
-  notify "hyprpicker is not installed"
+  notify-send "Color Picker" "hyprpicker yüklü değil."
   exit
 }
+
 killall -q hyprpicker
 color=$(hyprpicker)
+[ -z "$color" ] && exit
 
-check wl-copy && {
-  echo "$color" | sed -z 's/\n//g' | wl-copy
-}
+check wl-copy && echo "$color" | tr -d '\n' | wl-copy
 
-prevColors=$(head -n $((limit - 1)) "$loc/colors")
-echo "$color" >"$loc/colors"
-echo "$prevColors" >>"$loc/colors"
+{
+  echo "$color"
+  head -n $((limit - 1)) "$loc/colors"
+} > "$loc/colors"
+
 sed -i '/^$/d' "$loc/colors"
-source ~/.cache/wal/colors.sh && notify-send "Color Picker" "This color has been selected: $color" -i $wallpaper
+
+source ~/.cache/wal/colors.sh 2>/dev/null
+notify-send "Color Picker" "This color has been selected: $color" -i "${wallpaper:-color-picker}"
+
 pkill -RTMIN+1 waybar
