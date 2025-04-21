2f65ad82d55b3dc61246f068e931b6660338098a
Venxe <ivenxe@gmail.com>
2025-04-21 23:49:07 +0300

    Update install.sh

--- installers/install.sh
+++ installers/install.sh
@@ -49,8 +49,8 @@ fi
 echo "${CYAN}Installing Yay packages...${RESET}"
 xargs -a installers/yay-packages.txt -r yay -S --needed --noconfirm || error_exit "Failed to install Yay packages!"
 
-echo "${CYAN}Removing Dolphin and Htop...${RESET}"
-sudo pacman -Rns dolphin htop --noconfirm
+echo "${CYAN}Removing Dolphin...${RESET}"
+sudo pacman -Rns dolphin --noconfirm
 
 echo "${CYAN}Creating necessary directories...${RESET}"
 mkdir -p ~/Desktop ~/Documents ~/Downloads ~/Games ~/Music ~/Public ~/Templates ~/Videos
