74aeae0633026e10f153a33e40bfd41c63c906b4
Venxe <ivenxe@gmail.com>
2025-04-27 21:09:18 +0300

    Update set-theme.sh

--- dotcfg/set-theme.sh
+++ dotcfg/set-theme.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
 
-eval $(dbus-launch --sh-syntax)
+export XCURSOR_THEME="Qogir-manjaro-dark"
+export XCURSOR_SIZE=21
 
-# GNOME GSettings ayarları
+# GSettings - GNOME settings
 gsettings set org.gnome.desktop.interface gtk-theme "Materia-dark-compact"
 gsettings set org.gnome.desktop.interface icon-theme "kora"
 gsettings set org.gnome.desktop.interface cursor-theme "Qogir-manjaro-dark"
 gsettings set org.gnome.desktop.interface cursor-size 21
-gsettings set org.gnome.desktop.interface font-name "Fira Sans Regular 11"
+gsettings set org.gnome.desktop.interface font-name "Fira Sans 11"
 gsettings set org.gnome.desktop.interface font-hinting "slight"
 gsettings set org.gnome.desktop.interface font-antialiasing "grayscale"
 gsettings set org.gnome.desktop.interface font-rgba-order "rgb"
 gsettings set org.gnome.desktop.interface text-scaling-factor 1
 gsettings set org.gnome.desktop.interface toolbar-style "both-horiz"
 gsettings set org.gnome.desktop.interface toolbar-icons-size "small"
+gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 gsettings set org.gnome.desktop.sound event-sounds true
 gsettings set org.gnome.desktop.sound input-feedback-sounds false
-gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 
-# GTK3/GTK4 ayar dosyaları
-mkdir -p ~/.config/gtk-{3.0,4.0}
+# GTK3 & GTK4 Config
+mkdir -p ~/.config/gtk-3.0 ~/.config/gtk-4.0
 cat > ~/.config/gtk-3.0/settings.ini <<EOF
 [Settings]
 gtk-theme-name=Materia-dark-compact
@@ -37,11 +38,13 @@ gtk-xft-antialias=1
 gtk-xft-hinting=1
 gtk-xft-hintstyle=hintslight
 gtk-xft-rgba=rgb
+gtk-application-prefer-dark-theme=1
 EOF
 
+# Link GTK3 settings to GTK4
 ln -sf ~/.config/gtk-3.0/settings.ini ~/.config/gtk-4.0/settings.ini
 
-# GTK2 ayar dosyası
+# GTK2 Settings
 cat > ~/.gtkrc-2.0 <<EOF
 gtk-theme-name="Materia-dark-compact"
 gtk-icon-theme-name="kora"
@@ -60,12 +63,10 @@ gtk-xft-hintstyle="hintslight"
 gtk-xft-rgba="rgb"
 EOF
 
-# İmleç teması için fallback ayarı
+# Cursor Settings
 mkdir -p ~/.local/share/icons/default
 cat > ~/.local/share/icons/default/index.theme <<EOF
 [Icon Theme]
 Name=Default
 Inherits=Qogir-manjaro-dark
 EOF
-
-echo "Tüm tema ayarları başarıyla uygulandı!"
